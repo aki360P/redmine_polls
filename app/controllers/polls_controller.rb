b199aaf78995759fd76e3c11f725d05ab4a85ab0
Shem Pasamba <shemgp@aiias.edu>
2017-07-13 16:36:16 +0800

    Make it work for 3.3.0

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -4,13 +4,14 @@ class PollsController < ApplicationController
   before_filter :find_project,:authorize
 
   def index
-    @polls = VotingPoll.find_all_by_project_id(@project.id)
+    @polls = VotingPoll.where(:project_id => @project.id)
   end
 
   def new
     @poll = VotingPoll.new(:project_id => @project.id)
     if request.post? || request.put?
-      @poll.attributes = params[:poll]
+      unlocked_params = ActiveSupport::HashWithIndifferentAccess.new(params[:poll])
+      @poll.attributes = unlocked_params
       if @poll.save
         redirect_to :action => 'index', :project_id => @project
       end
@@ -25,16 +26,19 @@ class PollsController < ApplicationController
 
   def edit
     @poll = VotingPoll.find(params[:id])
-    if (request.post?  || request.put?) && @poll.update_attributes(params[:poll])
+    unlocked_params = ActiveSupport::HashWithIndifferentAccess.new(params[:poll])
+    if (request.post?  || request.put?) && @poll.update_attributes(unlocked_params)
       redirect_to :action => 'index', :project_id => @project
     end
   end
 
   def new_choice
     @poll = VotingPoll.find(params[:poll_id])
+    @choice = VotingChoice.new()
     if request.post? || request.put?
       @choice = VotingChoice.new()
-      @choice.attributes = params[:choice]
+      unlocked_params = ActiveSupport::HashWithIndifferentAccess.new(params[:choice])
+      @choice.attributes = unlocked_params
       if @choice.save
         redirect_to :action => 'index', :project_id => @project
       end
@@ -51,7 +55,8 @@ class PollsController < ApplicationController
     @choice = VotingChoice.find(params[:id])
     @poll = VotingPoll.find(params[:poll_id])
     if request.post? || request.put?
-      if @choice.update_attributes(params[:choice])
+      unlocked_params = ActiveSupport::HashWithIndifferentAccess.new(params[:choice])
+      if @choice.update_attributes(unlocked_params)
         redirect_to :action => 'index', :project_id => @project
       end
     end
@@ -59,9 +64,9 @@ class PollsController < ApplicationController
 
   def vote
     choice = VotingChoice.find(params[:choice_id])
-    exist_vote = VotingVote.find_by_user_id_and_poll_id(User.current.id, choice.poll.id)
+    exist_vote = VotingVote.where(:user_id => User.current.id, :poll_id => choice.poll.id)
     if !exist_vote or choice.poll.revote
-      exist_vote.destroy if exist_vote
+      exist_vote.destroy(exist_vote) if exist_vote.count >= 1
       vote = VotingVote.new
       vote.user_id = User.current.id
       vote.poll_id = choice.poll.id;
@@ -75,7 +80,7 @@ class PollsController < ApplicationController
   def reset_vote
     poll = VotingPoll.find(params[:poll_id])
     if poll.revote
-      vote = VotingVote.find_by_user_id_and_poll_id(User.current.id, params[:poll_id])
+      vote = VotingVote.where(:user_id => User.current.id, :poll_id => params[:poll_id])
       vote.destroy if vote
     end
     back_url = CGI.unescape(params[:back_url].to_s)
